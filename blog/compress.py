f27139cf3c8f895342a83ab0e0dd9bc274475f8f
geek-id <adhitya.putra1903@gmail.com>
2020-09-27 11:19:52 +0700

    add compression image

--- blog/compress.py
+++ blog/compress.py
@@ -3,11 +3,11 @@ from PIL import Image
 from django.core.files import File
 
 def compress(featured_image):
-    im = Image.open(featured_image)
+    im = Image.open(featured_image).convert("RGB")
     # create a BytesIO object
     im_io = BytesIO() 
     # save image to BytesIO object
-    im.save(im_io, 'PNG', quality=70) 
+    im.save(im_io, "WEBP", quality=70) 
     # create a django-friendly Files object
     new_image = File(im_io, name=featured_image.name)
     return new_image
